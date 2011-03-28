577b4a1a7d249c92d42a5c5a07e09d345b7db77e
madis.nomme <madis.nomme@04130996-0a6d-9396-48d0-11b1f4b11252>
2011-03-28 15:17:23 +0000

    Removed the namespace, everything is global now. Gogogogogogogoooo

--- poker/scripts/Poker.js
+++ poker/scripts/Poker.js
@@ -1,17 +1,13 @@
-/**
- * 
- */
 
-var Poker = new function() { // Our namespace
-	function Player(name, money, seat) {
+Player = function(name, money, seat) {
 		this.BBSize = 2;
 		this.name = name;
 		this.money = money;
 		this.seat = seat;
-	}
-
-	Player.prototype = {
-		constructor : Player,
+	};
+	
+Player.prototype = {
+		constructor : this.Player,
 		postSB : function() {
 			// TODO: it would be better to have TableManager responsible of carrying out this action
 			money -= BBSize / 2;
@@ -19,20 +15,20 @@ var Poker = new function() { // Our namespace
 		}
 	};
 
-	// There should be only 1 table manager per page
-	function TableManager() {
-		this.players = new Array(); // Holds player objects
-		this.allInPlayers = new Array(); // When a player is all in he would be taken out of this.players and be put here
-		this.actions = new Array(); // Will hold a array of actions. All actions concerning the game and players should be defined as functions in TableManager.actions
-		this.pot = 0; // Sum of all the bets thus far
-		this.board = new Array(); // Array of cards. When table is being drawn or new cards dealt, this is where the cards are asked/put.
-		
+// There should be only 1 table manager per page
+function TableManager() {
+	this.players = new Array(); // Holds player objects
+	this.allInPlayers = new Array(); // When a player is all in he would be taken out of this.players and be put here
+	this.actions = new Array(); // Will hold a array of actions. All actions concerning the game and players should be defined as functions in TableManager.actions
+	this.pot = 0; // Sum of all the bets thus far
+	this.board = new Array(); // Array of cards. When table is being drawn or new cards dealt, this is where the cards are asked/put.		
 	}
 	
-	function positionDealerButton() {
+function positionDealerButton() {
 		alert('Positioning dealer button');
 	}
-	TableManager.prototype = {
+	
+TableManager.prototype = {
 		constructor : TableManager,
 		startGame : function() {
 			positionDealerButton();
@@ -47,5 +43,4 @@ var Poker = new function() { // Our namespace
 //			fifthStreetBetting();
 //			payWinner();
 		}
-	};
 };
