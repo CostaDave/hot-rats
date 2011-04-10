e23c435f8725df2b9a7044158174ca56a5bb05c2
mihkel.vunk@gmail.com <mihkel.vunk@gmail.com@04130996-0a6d-9396-48d0-11b1f4b11252>
2011-04-10 15:46:55 +0000

    Lisasin meetodeid

--- trunk/poker-server/src/main/java/ee/ut/veebirakendused/pokerserver/Deck.java
+++ trunk/poker-server/src/main/java/ee/ut/veebirakendused/pokerserver/Deck.java
@@ -1,23 +1,38 @@
 package ee.ut.veebirakendused.pokerserver;
 
+import java.util.Vector;
+
 public class Deck {
-	private Card[] cards;
+	private Vector <Card> cards = new Vector<Card>();
 	private int nextCardIndex = 0;
 	
+	private String [] suits = {"c","d","h","s"};
+	private String [] ranks = {"2","3","4","5","6","7","8","9","T","J","Q","K","A"};
+	
+	Deck(){
+		for (int i=0;i<suits.length;i++){
+			for (int j = 0; j<ranks.length;j++){
+				cards.add(new Card(ranks[j], suits[i]));
+			}
+		}
+	}
+	public void shuffle(){
+		int nextCardIndex = 0;
+		int deckSize = cards.size();
+		for (int i = 0; i < deckSize; i++){
+			Card card = cards.elementAt(i);
+			int randIndex = (int) Math.floor(Math.random()*deckSize);
+			Card randCard = cards.elementAt(randIndex);
+			cards.elementAt(i).setRank(randCard.getRank());
+			cards.elementAt(i).setSuit(randCard.getSuit());
+			cards.elementAt(randIndex).setRank(card.getRank());
+			cards.elementAt(randIndex).setSuit(card.getSuit());
+		}
+	}
 
 }
 
 /*
- * Deck = function() {
-	this.cards = new Array();
-	this.nextCardIndex = 0;
-	for (rank in RANKS) {
-		for (suit in SUITS) {
-			this.cards.push(new Card(RANKS[rank], SUITS[suit]));
-		}
-	}
-};
-
 Deck.prototype = {
 	constructor : this.Deck,
 	shuffle : function() {
